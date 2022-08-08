1de79a14ab91daca7b505bcbc51729a9354f09d3
Kirill Shantin <shantin21@gmail.com>
2022-08-08 14:03:50 +0600

    Added loop for minting

--- tasks/mint.ts
+++ tasks/mint.ts
@@ -6,11 +6,14 @@ task("mint", "Mints from the NFT contract")
   .setAction(async function (taskArguments, hre) {
     const contract = await getContract("DinoFT", hre);
     if (contract) {
-      //for (let i = 1; i <= 50; i++) {
-      const transactionResponse = await contract.mintTo(taskArguments.address, {
-        gasLimit: 500_000,
-      });
-      console.log(`Transaction Hash: ${transactionResponse.hash}`);
-      //}
+      for (let i = 1; i <= 47; i++) {
+        const transactionResponse = await contract.mintTo(
+          taskArguments.address,
+          {
+            gasLimit: 500_000,
+          },
+        );
+        console.log(`Transaction Hash: ${transactionResponse.hash}`);
+      }
     }
   });
